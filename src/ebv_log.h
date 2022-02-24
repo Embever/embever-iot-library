b56324ad87fa955192d30f510a7c8a01fd10c893
Zoltan Ban <ban@embever.com>
2022-02-24 16:47:35 +0100

    Log module

---
+++ src/ebv_log.h
@@ -0,0 +1,30 @@
+#ifndef INC_EBV_LOG_H
+#define INC_EBV_LOG_H
+
+#include "ebv_log_conf.h"
+
+#if LOG_MODULE_NAME == EBV_ESP_LOG_NAME
+    #if EBV_ESP_LOG_EN == 1
+        #define MODULE_LOG_EN 1
+    #else
+        #define MODULE_LOG_EN 0
+    #endif
+#elif LOG_MODULE_NAME == EBV_IOT_LOG_NAME
+    #if EBV_IOT_LOG_EN == 1
+        #define MODULE_LOG_EN 1
+    #else
+        #define MODULE_LOG_EN 0
+    #endif
+#endif
+
+#if MODULE_LOG_EN == 1
+    #define DEBUG_EN 1
+    #define DEBUG_MSG_TRACE(...) p("%s:%d in %s() --> ", __FILE__, __LINE__, __FUNCTION__); p(__VA_ARGS__); p("\n\r")
+    #define DEBUG_MSG(...) p(__VA_ARGS__)
+#else
+    #define DEBUG_EN 0
+    #define DEBUG_MSG_TRACE(...)
+    #define DEBUG_MSG(...)
+#endif
+
+#endif
