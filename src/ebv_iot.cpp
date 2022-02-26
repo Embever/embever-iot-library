98354239ae782fc0ad0f8487f2772dd697099956
Zoltan Ban <ban@embever.com>
2022-02-26 08:45:48 +0100

    Overall update Logging New function return value schema More static buffer Logic changes

--- src/ebv_iot.cpp
+++ src/ebv_iot.cpp
@@ -129,6 +129,9 @@ ebv_ret_t ebv_iot_receiveAction(esp_response_t *response){
             memcpy( &response->response[0], &resp.response[6], 2);
 #if DEBUG_EN == 1
             uint16_t err_code = (uint16_t) resp.response[6] | (uint16_t) resp.response[7] << 8;
+            response->has_error_code = true;
+            memcpy( &response->response[0], &resp.response[6], 2);
+#if DEBUG_EN == 1
             switch (err_code){
                 case ESP_ERR_INVALID_CMD_DATA:
                     DEBUG_MSG_TRACE("Invalid data");
@@ -247,6 +250,9 @@ ebv_ret_t ebv_iot_submitActionResult(ebv_action_t *a, esp_response_t *response){
             memcpy( &response->response[0], &resp.response[6], 2);
 #if DEBUG_EN == 1
             uint16_t err_code = (uint16_t) resp.response[6] | (uint16_t) resp.response[7] << 8;
+            response->has_error_code = true;
+            memcpy( &response->response[0], &resp.response[6], 2);
+#if DEBUG_EN == 1
             switch (err_code){
                 case ESP_ERR_INVALID_CMD_DATA:
                     DEBUG_MSG_TRACE("Invalid data");
